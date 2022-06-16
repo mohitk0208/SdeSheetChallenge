6628fd87bfb81dbf7b1aa81c6309eb7fe2fd6da3
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-06-16 11:05:41 +0530

    added comments

--- Day_3_Arrays_part_3/1_search_in_2d_matrix.py
+++ Day_3_Arrays_part_3/1_search_in_2d_matrix.py
@@ -13,11 +13,12 @@ codestudio : https://www.codingninjas.com/codestudio/problems/980531?topList=str
 
 # leetcode solution
 # use binary search to find the target
+# flatten the matrix and use binary search to find the target
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m, n = len(matrix), len(matrix[0])
 
-        l = m * n
+        l = m * n               # total elements
 
         low = 0
         high = l - 1
@@ -25,16 +26,16 @@ class Solution:
         while low <= high:
             mid = (low + high) // 2
 
-            i = mid // n
-            j = mid % n
+            i = mid // n                # find row index using element index in the flattened matrix
+            j = mid % n                 # find column index using element index in the flattened matrix
 
             if matrix[i][j] == target:
                 return True
 
-            if matrix[i][j] < target:
+            if matrix[i][j] < target:   # now search in the right half
                 low = mid + 1
-            else:
+            else:                       # now search in the left half
                 high = mid - 1
 
 
-        return False
+        return False            # not found
