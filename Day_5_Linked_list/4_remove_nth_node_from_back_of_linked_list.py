a5e2509c03760df74179397fce855c90ef88cf98
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-06-18 15:43:44 +0530

    solved remove nth node from the bac of linked list

---
+++ Day_5_Linked_list/4_remove_nth_node_from_back_of_linked_list.py
@@ -0,0 +1,33 @@
+'''
+#################### Remove Nth Node From End of Linked List ####################
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+leetcode : https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+'''
+
+# solution
+# SC -> O(1) and TC -> O(n)
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if head == None:
+            return head
+
+        slow = head
+        fast = head
+
+        for _ in range(n):
+            fast = fast.next
+
+        if fast == None:
+            slow = head.next
+            return slow
+
+        while fast.next != None:
+            slow = slow.next
+            fast = fast.next
+
+
+        slow.next = slow.next.next
+
+        return head
