06424ee944f61f0eb57cded58588cec47d44bfcd
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-06-19 16:37:36 +0530

    renamed a file name

--- Day_23_Graph/4_detect_cycle_in_undirected_graph_bfs.py
+++ Day_23_Graph/4_detect_cycle_in_undirected_graph_bfs.py
@@ -1,5 +1,5 @@
 '''
-################# Detect cycle in undirected graph #####################
+################# Detect cycle in undirected graph BFS #####################
 You have been given an undirected graph with 'N' vertices and 'M' edges. The vertices are labelled from 1 to 'N'.
 Your task is to find if the graph contains a cycle or not.
 A path that starts from a given vertex and ends at the same vertex traversing the edges only once is called a cycle.
