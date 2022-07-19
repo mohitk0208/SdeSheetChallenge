10947edcf306b61bbb812892228da1f1f9ffc5e7
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-07-19 18:41:02 +0530

    .

--- Day_14_stack_and_queue_part_2/6_implement_min_stack.py
+++ Day_14_stack_and_queue_part_2/6_implement_min_stack.py
@@ -47,3 +47,43 @@ class MinStack:
     # TC -> O(1)
     def getMin(self) -> int:
         return self.min[-1]
+
+
+
+
+# approach 2:
+# SC -> O(n) + O(1)
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = float("inf")
+
+    def push(self, val: int) -> None:
+        if len(self.stack) == 0:
+            self.stack.append(val)
+            self.min = val
+        else:
+            if val >= self.min:
+                self.stack.append(val)
+            else:
+                self.stack.append(2*val - self.min)
+                self.min = val
+
+
+    def pop(self) -> None:
+        if self.stack[-1] >= self.min:
+            self.stack.pop()
+        else:
+            self.min = 2*self.min - self.stack[-1]
+            self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack[-1] >= self.min:
+            return self.stack[-1]
+        else:
+            return self.min
+
+
+    def getMin(self) -> int:
+        return self.min
